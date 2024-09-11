0fba1469bfaa65160b14391c24f6b157596e4420
NathanaelLee15 <NathanaelLee15@gmail.com>
2024-09-11 18:54:27 -0400

    Add help functionality with navigation, editing, selecting text, clipboard, and undo features

---
+++ help.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/rivo/tview"
+)
+
+func CreateHelpItems() (*tview.TextView, *tview.TextView, *tview.TextView) {
+	help1 := tview.NewTextView().
+		SetDynamicColors(true).
+		SetText(`[green]Navigation
+
+[yellow]Left arrow[white]: Move left.
+[yellow]Right arrow[white]: Move right.
+[yellow]Down arrow[white]: Move down.
+[yellow]Up arrow[white]: Move up.
+[yellow]Ctrl-A, Home[white]: Move to the beginning of the current line.
+[yellow]Ctrl-E, End[white]: Move to the end of the current line.
+[yellow]Ctrl-F, page down[white]: Move down by one page.
+[yellow]Ctrl-B, page up[white]: Move up by one page.
+[yellow]Alt-Up arrow[white]: Scroll the page up.
+[yellow]Alt-Down arrow[white]: Scroll the page down.
+[yellow]Alt-Left arrow[white]: Scroll the page to the left.
+[yellow]Alt-Right arrow[white]:  Scroll the page to the right.
+[yellow]Alt-B, Ctrl-Left arrow[white]: Move back by one word.
+[yellow]Alt-F, Ctrl-Right arrow[white]: Move forward by one word.
+
+[blue]Press Enter for more help, press Escape to return.`)
+	help2 := tview.NewTextView().
+		SetDynamicColors(true).
+		SetText(`[green]Editing[white]
+
+Type to enter text.
+[yellow]Ctrl-H, Backspace[white]: Delete the left character.
+[yellow]Ctrl-D, Delete[white]: Delete the right character.
+[yellow]Ctrl-K[white]: Delete until the end of the line.
+[yellow]Ctrl-W[white]: Delete the rest of the word.
+[yellow]Ctrl-U[white]: Delete the current line.
+
+[blue]Press Enter for more help, press Escape to return.`)
+	help3 := tview.NewTextView().
+		SetDynamicColors(true).
+		SetText(`[green]Selecting Text[white]
+
+Move while holding Shift or drag the mouse.
+Double-click to select a word.
+[yellow]Ctrl-L[white] to select entire text.
+
+[green]Clipboard
+
+[yellow]Ctrl-Q[white]: Copy.
+[yellow]Ctrl-X[white]: Cut.
+[yellow]Ctrl-V[white]: Paste.
+		
+[green]Undo
+
+[yellow]Ctrl-Z[white]: Undo.
+[yellow]Ctrl-Y[white]: Redo.
+
+[blue]Press Enter for more help, press Escape to return.`)
+	return help1, help2, help3
+}
